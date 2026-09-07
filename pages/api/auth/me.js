5e71400e2dfbb54cba10c96120763eeec5483c42
Eswrsxe <damasiolopesrennan2@gmail.com>
2026-09-07 01:22:42 -0300

    update pages/api/auth/me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
         discordId: true,
         username: true,
         avatar: true,
+        points: true,
         _count: { select: { passkeys: true } },
       },
     });
